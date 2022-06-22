0e3581c90d0c1e7641eac90d48464870f8d0d515
Fabian Gabel <fabian.gabel@tuhh.de>
2022-06-22 15:45:01 +0200

    minimal setup for bootstrap tour

---
+++ static/js/pontifex-tour.js
@@ -0,0 +1,29 @@
+$(function() {
+  
+  // define tour
+  var tour = new Tour({
+    debug: true,
+    basePath: location.pathname.slice(0, location.pathname.lastIndexOf('/')),
+    steps: [{
+      element: "#cy",
+      title: "The Network",
+      content: "Visualizes the network"
+    }, {
+      path: "/newPage.html",
+      element: "#my-other-element",
+      title: "Title of my step",
+      content: "Content of my step"
+    }]
+  });
+
+  // init tour
+  tour.init();
+
+  // start tour
+  $('#start-tour').click(function() {
+    tour.restart();
+  });
+  
+
+});
+
